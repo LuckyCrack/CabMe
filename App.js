27ca1936eb25453ae804f6a0216bcaabce75612e
LuckyCrack <vaibhavnanda2000@gmail.com>
2022-04-13 22:41:01 +0530

    Inital Commit and added Redux

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Provider } from "react-redux";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 16,
+  },
+});
